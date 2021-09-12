fde18594a7ba6e19243babb7b78818ed8ea39f69
yvonne <dudeitsjane@gmail.com>
2021-09-12 16:49:56 -0400

    init

--- bot.py
+++ bot.py
@@ -44,11 +44,6 @@ async def load(ctx, extension):
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
-"""
-for filename in os.listdir('./cogs'):
-    if filename.endswith(".py"):
-        client.load_extension(f'cogs.{filename[:-3]}')"""
-
 extensions = ['cogs.ServerInfo', 'cogs.Administrator', 'cogs.Help', 'cogs.Fun', 'cogs.WebRequests']
 if __name__ == '__main__':
     for ext in extensions:
